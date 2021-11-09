3df07d2e4fdb04a458fddf2a80aea063b65f62c7
Dheeraj <gandhidheeraj9@gmail.com>
2021-11-10 00:03:50 +0530

    fixed chatbox, added dark grey colour on nav hover, font weight of nav increased

--- js/about.js
+++ js/about.js
@@ -1,22 +1,3 @@
-const toggleCollapse = document.querySelector(".toggle-collapse span");
-const nav = document.querySelector(".nav");
-// onclick event on toggle Collapse span tag
-toggleCollapse.onclick = (e) => {
-  nav.classList.toggle("collapse");
-  e.target.classList.toggle("toggle-click");
-};
-
-$(document).ready(function () {
-  $(window).scroll(function () {
-    // sticky navbar on scroll script
-    if (this.scrollY > 20) {
-      $(".nav").addClass("sticky");
-    } else {
-      $(".nav").removeClass("sticky");
-    }
-  });
-});
-
 // JQUERY FOR OWL CAROUSEL
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
@@ -52,28 +33,26 @@ $(document).ready(function () {
   });
 });
 
-$('.partner-image-div').owlCarousel({
+$(".partner-image-div").owlCarousel({
   center: true,
-  margin:10,
-  loop:true,
+  margin: 10,
+  loop: true,
   dots: false,
   autoplay: true,
   autoplaySpeed: 4000,
   smartSpeed: 4000,
   center: true,
-  responsive:{
-      0:{
-          items:1,
-          margin:0,
-
-      },
-      600:{
-          items:2,
-          margin:0,
-      },
-      1000:{
-          items:4,
-      }
-  }
-})
-
+  responsive: {
+    0: {
+      items: 1,
+      margin: 0,
+    },
+    600: {
+      items: 2,
+      margin: 0,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
